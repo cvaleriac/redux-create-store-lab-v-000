a950339960a2d904d9f5255be971d86e8040a207
valeria campilongo <cvaleriac@hotmail.it>
2019-10-15 17:36:05 +0000

    Automatically backed up by Learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,15 +1,15 @@
 export default function createStore(reducer) {
   let state;
- 
+
   function dispatch(action) {
     state = reducer(state, action);
     render();
   }
- 
+
   function getState() {
     return state;
   };
- 
+
   return {
     dispatch,
     getState
